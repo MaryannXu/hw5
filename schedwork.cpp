bdfc4ad38ca51766d4e4009cd8ce9c167e340adf
Maryann Xu <mxxu@usc.edu>
2023-04-05 13:54:26 -0700

    all done

--- schedwork.cpp
+++ schedwork.cpp
@@ -18,7 +18,106 @@ using namespace std;
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
+bool helper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, size_t day, size_t slot, vector<int>& scheduled_workers)
+{
+    // // Check if there are enough workers available on this day
+    // size_t available_workers = 0;
+    // for (size_t i = 0; i < avail.size(); ++i) {
+    //     if (avail[i][day] && !scheduled_workers[i]) {
+    //         ++available_workers;
+    //     }
+    // }
+    // if (available_workers < dailyNeed) {
+    //     return false;
+    // }
 
+    //base case
+    //filled the schedule for all days, return true
+    if (day == sched.size()) {
+        return true;
+    }
+
+    //try to schedule each worker for the current day
+    for (size_t i = 0; i < avail[0].size(); i++) {
+        //check if worker available and has not reached maxShifts
+        if (avail[day][i] && scheduled_workers[i] < maxShifts) {
+            //schedule the worker for the current day
+            //sched[day].push_back(i);
+            sched[day][slot] = i;
+            
+            //increment the worker's shift count
+            scheduled_workers[i]++;
+
+            //recursively try to fill the rest of the schedule
+            bool valid;
+            if (slot == dailyNeed-1){
+                valid = helper(avail, dailyNeed, maxShifts, sched, day + 1, 0, scheduled_workers);
+                return valid;
+            } 
+            else {
+                valid = helper(avail, dailyNeed, maxShifts, sched, day, slot+1, scheduled_workers);
+                return valid;
+            }
+            
+            /*
+            if slot == dailyNeed:
+                helper(avail, dailyNeed, maxShifts, sched, day + 1, 0, sched_workers)
+            else:
+                helper(avail, dailyNeed, maxShifts, sched, day, slot + 1, sched_workers)
+
+            */
+            
+
+            //unmark the worker as scheduled
+            scheduled_workers[i]--;
+            //remove the worker from the current day's schedule
+            sched[day].pop_back();
+        }
+    }
+
+    //tried all workers and none worked
+    return false;
+}
+
+bool schedule(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched
+)
+{
+    //check for invalid inputs
+    if (avail.empty() || dailyNeed <= 0 || dailyNeed > avail.size() || maxShifts <= 0) {
+        return false;
+    }
+
+    //initialize the schedule to the correct dimensions
+    sched = DailySchedule(avail.size());
+    for (size_t i = 0; i < sched.size(); i++){
+         sched[i].resize(dailyNeed);
+    }
+
+    // //make sure the avail matrix has the correct dimensions
+    // for (const auto& row : avail) {
+    //     if (row.size() != sched.size()) {
+    //         return false;
+    //     }
+    // }
+
+    //initialize the vector of scheduled workers to all false
+    vector<int> scheduled_workers(avail[0].size(), 0);
+
+    //start the recursive search to fill the schedule
+    return helper(avail, dailyNeed, maxShifts, sched, 0, 0, scheduled_workers);
+}
+
+
+
+
+
+
+
+/*
 // Add prototypes for any helper functions here
 
 
@@ -31,14 +130,10 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
-        return false;
-    }
-    sched.clear();
     // Add your code below
 
 
 
 
 }
-
+*/
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,68 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void generate_combinations(string fixed, string floating, int pos, set<string>& combinations) {
+    // std::cout << "This is the begining" << std::endl;
+    // std::cout << "fixed " << fixed << std::endl;
+    // std::cout << "floating " << floating << std::endl;
+    // std::cout << "pos " << pos+1 << std::endl;
+    if (pos == fixed.length()) {
+        combinations.insert(fixed);
+        return;
+    }
+    if (fixed[pos] != '-') {
+        generate_combinations(fixed, floating, pos+1, combinations);
+        return;
+    }
+    
+    // check if length of floating == number of blanks
+    int numBlanks = 0;
+    for (int i = 0; i < fixed.length(); i++){
+        if (fixed[i] == '-'){
+            numBlanks++;
+        }
+    }
 
+    // std::cout << floating.length() << " " << numBlanks << " " << fixed << std::endl;
+    if (floating.length() == numBlanks){
+        //  std::cout << "inside this loop " << std::endl;
+        //check all the floating chars in each of the blanks
+        for (int i = 0; i < floating.length(); i++) {
+            char c = floating[i];
+            fixed[pos] = floating[i];
+            // std::cout << "floating before " << floating << std::endl;
+            floating.erase(i, 1);
+            
+            // std::cout << "Inside loop" << std::endl;
+            // std::cout << "fixed " << fixed << std::endl;
+            // std::cout << "floating after delete " << floating << std::endl;
+            // std::cout << "pos " << pos+1 << std::endl;
+            generate_combinations(fixed, floating, pos+1, combinations);
+            floating.push_back(c);
+            // std::cout << "floating after add back " << floating << std::endl;
+        }
+    }
+    else {
+        //check entire alphabet
+        //if it exists in floating, delete the letter from floating
+        for (char c = 'a'; c <= 'z'; c++) {
+            for (int i = 0; i < floating.length(); i++){
+                if (floating[i] == c){
+                    floating.erase(i,1);
+                    fixed[pos] = c;
+                    generate_combinations(fixed, floating, pos+1, combinations);
+                    floating.push_back(c);
+                }
+            }
+                fixed[pos] = c;
+                generate_combinations(fixed, floating, pos+1, combinations);
+            }
+    }
+ }
+
+bool is_valid_word(const string& word, const set<string>& dict) {
+    return dict.find(word) != dict.end();
+}
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +83,37 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    
+// Initialize the set of all possible words to an empty set
+    std::set<std::string> words;
+    //std::cout<< "one " << std::endl;
+    // Check if the input string has any empty slots
+    size_t pos = in.find('-');
+    if (pos == std::string::npos) {
+        // If there are no empty slots, check if the input string is a valid word
+        if (is_valid_word(in, dict)) {
+            words.insert(in);
+        }
+        return words;
+    }
+    std::cout << "in words " << in << std::endl;
+    std::cout << "floating letters " << floating << std::endl;
+    
+    // Generate all possible combinations of characters in the remaining empty slots
+    std::set<std::string> combinations;
+    //std::cout<< "two " << std::endl;
+    generate_combinations(in, floating, 0, combinations);
+    //std::cout<< "three " << std::endl;
+    // Check if each combination is a valid English word and add it to the set of possible words
+    std::set<std::string>::iterator it;
+    for (it = combinations.begin(); it != combinations.end(); ++it) {
+        if (is_valid_word(*it, dict)) {
+            words.insert(*it);
+        }
+    }
+   // std::cout<< "four " << std::endl;
+    
+    return words;
 }
 
 // Define any helper functions here
